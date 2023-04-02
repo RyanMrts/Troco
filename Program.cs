17a433196fa69aebacb0dfe4a8daeee8e8d54e84
RyanMrts <ryanmartinssilva2006@gmail.com>
2023-04-02 20:30:02 -0300

    exercicio feito

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿double valor1, valor2, troco;
+Console.Write("Digite o valor de compra: ");
+valor1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o valor pago: ");
+valor2 = Convert.ToDouble(Console.ReadLine());
+
+troco = valor1 - valor2;
+
+Console.WriteLine($"Valor do troco: {troco:N2}");
